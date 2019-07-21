03b887ab27a64d619052d0ef247d5e5eb3f80bdb
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2019-07-21 09:53:00 +0200

    Erratum to ea2a4d2

--- setup-build.sh
+++ setup-build.sh
@@ -11,5 +11,3 @@ curl -o cygwin-setup-x86.exe -Lf https://cygwin.com/setup-x86.exe
 ./cygwin-setup-x86.exe -B -qnNd -R c:/cygwin -l c:/cygwin/var/cache/setup -s http://ftp.inf.tu-dresden.de/software/windows/cygwin32/ -P p7zip -P cygutils-extra -P make -P mingw64-i686-gcc-g++ -P mingw64-i686-gcc-core -P mingw64-i686-gcc -P patch -P rlwrap -P libreadline6 -P diffutils -P mingw64-i686-binutils -P m4
 
 echo "none /cygdrive cygdrive binary,posix=0,user,noacl 0 0" > c:/cygwin/etc/fstab
-
-fi
