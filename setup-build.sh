0c32cbc74d1beed807965e5f9461406be94d95e2
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2019-07-23 23:01:06 +0200

    Start from vfdeps-win-noz3 binary bundle

--- setup-build.sh
+++ setup-build.sh
@@ -7,6 +7,10 @@
 set -e # Stop as soon as a command fails.
 set -x # Print what is being executed.
 
+VFDEPS_NOZ3=vfdeps-14f6176-win-noz3.txz
+curl -o $VFDEPS_NOZ3 -Lf https://dl.bintray.com/verifast/verifast/$VFDEPS_NOZ3
+tar -C C:/ -xjf $VFDEPS_NOZ3
+
 curl -o cygwin-setup-x86.exe -Lf https://cygwin.com/setup-x86.exe
 ./cygwin-setup-x86.exe -B -qnNd -R c:/cygwin -l c:/cygwin/var/cache/setup -s http://ftp.inf.tu-dresden.de/software/windows/cygwin32/ -P p7zip -P cygutils-extra -P make -P mingw64-i686-gcc-g++ -P mingw64-i686-gcc-core -P mingw64-i686-gcc -P patch -P rlwrap -P libreadline6 -P diffutils -P mingw64-i686-binutils -P m4 -P curl -P python
 
