c543a7b97a5f4bded44311ee115beea4a3f712f8
NielsMommen <32457927+NielsMommen@users.noreply.github.com>
2021-10-28 23:03:46 +0200

    Cxx deps (#2)

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ VFDEPS_PARENT_DIR=C:/
 VFDEPS_PLATFORM=win
 
 VFDEPS_DIR=$VFDEPS_PARENT_DIR/$VFDEPS_DIRNAME
-# mkdir $VFDEPS_DIR
+mkdir $VFDEPS_DIR
 
 /c/cygwin/bin/bash -lc "cd /cygdrive/$BUILD_DIR && make PREFIX=$VFDEPS_DIR"
 
@@ -25,19 +25,5 @@ cd $VFDEPS_PARENT_DIR
 tar cjf $VFDEPS_FILEPATH $VFDEPS_DIRNAME
 cd $BUILD_DIR
 
-echo '{' > bintray.json
-echo '    "package": {' >> bintray.json
-echo '        "name": "vfdeps",' >> bintray.json
-echo '        "repo": "verifast",' >> bintray.json
-echo '        "subject": "verifast",' >> bintray.json
-echo '        "vcs_url": "https://github.com/verifast/vfdeps",' >> bintray.json
-echo '        "licenses": ["MIT"]' >> bintray.json
-echo '    },' >> bintray.json
-echo '    "version": {' >> bintray.json
-echo '        "name": "'$VFDEPS_VERSION'"' >> bintray.json
-echo '    },' >> bintray.json
-echo '    "files": [{"includePattern": "upload/(.*)", "uploadPattern": "$1"}],' >> bintray.json
-echo '    "publish": true' >> bintray.json
-echo '}' >> bintray.json
-
-cat bintray.json
+ls -l $VFDEPS_FILEPATH
+/c/cygwin/bin/bash -lc "sha224sum /cygdrive$VFDEPS_FILEPATH"
